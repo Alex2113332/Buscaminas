d64a773716665f991f785e5966e2f460a158824e
Alex <alejandro.fernandez@radmas.com>
2025-02-25 17:56:59 +0100

    feat: Implement cell composable in MainActivity.

--- app/src/main/java/com/example/buscaminas/ui/MainActivity.kt
+++ app/src/main/java/com/example/buscaminas/ui/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.border
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +31,7 @@ fun Cell() {
 
 @Composable
 fun MinesweeperScreen() {
-
+    Cell()
 }
 
 @Preview(showBackground = true)

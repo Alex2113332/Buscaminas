97dbfc6730d92af24d1ca9d41a7483543794b826
Alex <alejandro.fernandez@radmas.com>
2025-02-26 14:01:21 +0100

    feat: Implement controls bar with mines and time counter.

--- app/src/main/java/com/example/buscaminas/ui/Controls.kt
+++ app/src/main/java/com/example/buscaminas/ui/Controls.kt
@@ -1,27 +1,38 @@
 package com.example.buscaminas.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Controls() {
+    val minesRenaining = remember { mutableStateOf(5) }
+    val time = remember { mutableStateOf(0) }
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "Mines: 5"
+            text = "Mines: ${minesRenaining.value}"
         )
         IconButton(onClick = { Restart() }) {
             Icon(
@@ -30,6 +41,12 @@ fun Controls() {
                 tint = Color.Black
             )
         }
-        Text(text = "Time: 00:00")
+        Text(text = "Time: ${time.value}")
     }
+}
+
+@Preview
+@Composable
+fun ControlsPreview() {
+    Controls()
 }

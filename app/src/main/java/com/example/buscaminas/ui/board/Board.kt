d6e98b78e70cadac07910a827efa90388dd2dd72
Alex <alejandro.fernandez@radmas.com>
2025-02-26 11:36:16 +0100

    feat: Implement board with multiple cells.

--- app/src/main/java/com/example/buscaminas/ui/board/Board.kt
+++ app/src/main/java/com/example/buscaminas/ui/board/Board.kt
@@ -1,13 +1,18 @@
 package com.example.buscaminas.ui.board
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.buscaminas.ui.cell.Cell
 import com.example.buscaminas.ui.cell.State
 
 @Composable
-fun Board() {
-    Cell(State.Hidden)
+fun Board(width: Int = 6) {
+    Row {
+        repeat(width) {
+            Cell(State.Hidden)
+        }
+    }
 }
 
 @Preview(showBackground = true)

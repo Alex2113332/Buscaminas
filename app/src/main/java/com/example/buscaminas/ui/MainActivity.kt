56634b6a211182dbe956dbbde7b1e215c8497661
Alex <alejandro.fernandez@radmas.com>
2025-02-26 13:22:28 +0100

    feat: Implement game header, restart button and board in MainActivity.

--- app/src/main/java/com/example/buscaminas/ui/MainActivity.kt
+++ app/src/main/java/com/example/buscaminas/ui/MainActivity.kt
@@ -3,8 +3,25 @@ package com.example.buscaminas.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buscaminas.domain.CellState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +34,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MinesweeperScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Mines:"
+            )
+            Icon(
+                imageVector = Icons.Default.Face,
+                contentDescription = "Happy face",
+                tint = Color.Black
+            )
+            Text(text = "Time:")
+        }
+        Board(List(6) { List(7) { CellState.Hidden } })
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+        ) {
+            Text(text = "Restart")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)

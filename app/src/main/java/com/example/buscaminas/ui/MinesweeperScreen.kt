7fd75690e69c21ca05f3bb797aec15f70841f8d6
Alex <alejandro.fernandez@radmas.com>
2025-02-26 13:47:08 +0100

    feat: Add Controls composable and background to MinesweeperScreen.

--- app/src/main/java/com/example/buscaminas/ui/MinesweeperScreen.kt
+++ app/src/main/java/com/example/buscaminas/ui/MinesweeperScreen.kt
@@ -1,5 +1,6 @@
 package com.example.buscaminas.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,47 +9,53 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.buscaminas.domain.CellState
 
 @Composable
 fun MinesweeperScreen() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(20.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(20.dp),
 
         ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Mines: 5"
-            )
-            Icon(
-                imageVector = Icons.Default.Face,
-                contentDescription = "Happy face",
-                tint = Color.Black
-            )
-            Text(text = "Time: 00:00")
-        }
+        Controls()
         Board(List(6) { List(7) { CellState.Hidden } })
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
-        ) {
-            Text(text = "Restart")
-        }
-
     }
+}
+
+@Composable
+private fun Controls() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Mines: 5"
+        )
+        Icon(
+            imageVector = Icons.Default.Face,
+            contentDescription = "Happy face",
+            tint = Color.Black
+        )
+        Text(text = "Time: 00:00")
+    }
+}
+
+@Preview
+@Composable
+fun MinesweeperScreenPreview() {
+    MinesweeperScreen()
 }

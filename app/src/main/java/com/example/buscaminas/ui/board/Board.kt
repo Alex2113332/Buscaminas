fbc45da56f37ec40f68ad4e88b8f0212f0e9eaad
Alex <alejandro.fernandez@radmas.com>
2025-02-26 11:43:59 +0100

    feat: Implement Board composable with rows and columns.

--- app/src/main/java/com/example/buscaminas/ui/board/Board.kt
+++ app/src/main/java/com/example/buscaminas/ui/board/Board.kt
@@ -1,5 +1,6 @@
 package com.example.buscaminas.ui.board
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -7,10 +8,14 @@ import com.example.buscaminas.ui.cell.Cell
 import com.example.buscaminas.ui.cell.State
 
 @Composable
-fun Board(width: Int = 6) {
-    Row {
-        repeat(width) {
-            Cell(State.Hidden)
+fun Board(width: Int = 6 , height: Int = 6) {
+    Column {
+        repeat(height) {
+            Row {
+                repeat(width) {
+                    Cell(State.Hidden)
+                }
+            }
         }
     }
 }

d9c75c4b96acce83c996713a371ec5ccbccf381b
Alejandro <alejandro.fernandez@radmas.com>
2025-03-10 19:00:07 +0100

    feat: Refactor DifficultySaver to support custom difficulty

--- app/src/main/java/com/example/buscaminas/ui/DifficultySaver.kt
+++ app/src/main/java/com/example/buscaminas/ui/DifficultySaver.kt
@@ -3,22 +3,48 @@ package com.example.buscaminas.ui
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.Saver
-import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.saveable.mapSaver
+import com.example.buscaminas.domain.CustomDifficulty
 import com.example.buscaminas.domain.Difficulty
 import com.example.buscaminas.domain.EasyDifficulty
 import com.example.buscaminas.domain.HardDifficulty
 import com.example.buscaminas.domain.MediumDifficulty
 
-object DifficultySaver : Saver<MutableState<Difficulty>, String> {
-    override fun restore(value: String) = mutableStateOf(
-        when (value) {
-            EasyDifficulty::class.simpleName.orEmpty() -> EasyDifficulty
-            MediumDifficulty::class.simpleName.orEmpty() -> MediumDifficulty
-            HardDifficulty::class.simpleName.orEmpty() -> HardDifficulty
-            else -> error("Invalid difficulty: $value")
-        }
-    )
+private const val TYPE = "type"
+private const val EASY = "easy"
+private const val MEDIUM = "medium"
+private const val HARD = "hard"
+private const val ROWS = "rows"
+private const val COLS = "cols"
+private const val PERCENT = "percent"
 
-    override fun SaverScope.save(value: MutableState<Difficulty>) =
-        value.value::class.simpleName.orEmpty()
-}
+val DifficultySaver: Saver<MutableState<Difficulty>, Any> = mapSaver(
+    save = {
+        when (val difficulty = it.value) {
+            is EasyDifficulty -> mapOf(TYPE to EASY)
+            is MediumDifficulty -> mapOf(TYPE to MEDIUM)
+            is HardDifficulty -> mapOf(TYPE to HARD)
+            is CustomDifficulty -> mapOf(
+                ROWS to difficulty.rows,
+                COLS to difficulty.columns,
+                PERCENT to difficulty.minePercentage,
+            )
+        }
+    },
+    restore = { data ->
+        mutableStateOf(
+            when (data[TYPE]) {
+                EASY -> EasyDifficulty
+                MEDIUM -> MediumDifficulty
+                HARD -> HardDifficulty
+                null -> CustomDifficulty(
+                    rows = (data[ROWS] as Int),
+                    columns = (data[COLS] as Int),
+                    minePercentage = (data[PERCENT] as Float)
+                )
+
+                else -> error("Unknown difficulty type")
+            }
+        )
+    }
+)
